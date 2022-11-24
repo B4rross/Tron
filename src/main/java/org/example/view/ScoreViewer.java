303391f830c4ba95da6fa6407bd1bb85f0c7dbd8
unknown <up202108706@up.pt>
2022-11-24 22:28:24 +0000

    Added scoreboard/Game now resets when collision

--- src/main/java/org/example/view/ScoreViewer.java
+++ src/main/java/org/example/view/ScoreViewer.java
@@ -4,15 +4,15 @@ import org.example.gui.GUI;
 import org.example.model.Position;
 
 public class ScoreViewer {
-    public void draw(GUI gui, int s1, int width,int s2){
+    public void draw(GUI gui, int s2, int width,int s1){
         String string="";
         String score="";
         for(int i=0; i < width/2-5; i++){
             string=string.concat("H");
         }
-        score=score.concat(Integer.toString(s1));
-        score=score.concat(" - ");
         score=score.concat(Integer.toString(s2));
+        score=score.concat(" - ");
+        score=score.concat(Integer.toString(s1));
 
         gui.drawText(new Position(0,0),string,"#FFFFFF");
         gui.drawText(new Position(width/2-3,0),score,"#FFFFFF");
